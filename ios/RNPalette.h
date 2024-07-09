5bf790cbcbc8d891601817aac5a2ddb72b463161
王锐彬 <492960423@qq.com>
2024-07-09 15:00:47 +0800

    1

--- ios/RNPalette.h
+++ ios/RNPalette.h
@@ -1,15 +1,5 @@
-
-#if __has_include("RCTImageLoader.h")
-#import "RCTImageLoader.h"
-#else
 #import <React/RCTImageLoader.h>
-#endif
-
-#if __has_include("RCTEventEmitter.h")
-#import "RCTEventEmitter.h"
-#else
 #import <React/RCTEventEmitter.h>
-#endif
 
 #import <GLKit/GLKit.h>
 
